7d4d4d93453a641eadc116a0d2495d07f1a51882
Hardik Pithva <hardikpthv@gmail.com>
2019-01-27 03:17:38 +0530

    feat: add header component

---
+++ src/app/header/header.component.js
@@ -0,0 +1,13 @@
+/**
+ * Header component
+ * @class sidenav
+ * @example
+ * <app-nav></app-nav>
+ */
+(function() {
+  "use strict";
+  angular.module("quicargo").component("appNav", {
+    templateUrl: "header/header.html",
+    controller: function HeaderController() {}
+  });
+})();
