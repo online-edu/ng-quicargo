e13f8c653ebd34a5e81c80236fac56320d449bf0
Hardik Pithva <hardikpthv@gmail.com>
2019-01-27 22:03:20 +0530

    feat: add delivery and history component

---
+++ src/app/delivery/delivery.component.js
@@ -0,0 +1,13 @@
+/**
+ * Delivery component
+ * @class delivery
+ * @example
+ * <delivery></delivery>
+ */
+(function() {
+  "use strict";
+  angular.module("quicargo").component("delivery", {
+    templateUrl: "partials/delivery/delivery.html",
+    controller: function DeliveryController() {}
+  });
+})();
---
+++ src/app/history/history.component.js
@@ -0,0 +1,13 @@
+/**
+ * History component
+ * @class history
+ * @example
+ * <history></history>
+ */
+(function() {
+  "use strict";
+  angular.module("quicargo").component("history", {
+    templateUrl: "partials/history/history.html",
+    controller: function HistoryController() {}
+  });
+})();
