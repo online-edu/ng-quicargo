b30e8346e4c763b29af776e4934df5090b8a357f
Hardik Pithva <hardikpthv@gmail.com>
2019-01-28 01:54:26 +0530

    feat: setup karma and jasmine

---
+++ src/app/sidenav/sidenav.component.spec.js
@@ -0,0 +1,29 @@
+describe("Component: sidenav", function() {
+  beforeEach(module("quicargo"));
+  var controller;
+  var scope;
+  var mockItems = [
+    {
+      link: "dashboard",
+      title: "New Delivery",
+      disabled: false
+    }
+  ];
+
+  describe("HeaderController", function() {
+    beforeEach(inject(function($rootScope, $componentController) {
+      scope = $rootScope.$new();
+      controller = $componentController("sidenav", { $scope: scope });
+    }));
+
+    it("should have property 'navItems' defined", function() {
+      expect(controller.navItems).toBeDefined();
+      expect(controller.navItems.length).toBe(3);
+    });
+
+    it("should have property 'collapsed' false", function() {
+      expect(controller.collapsed).toBeDefined();
+      expect(controller.collapsed).toBe(false);
+    });
+  });
+});
