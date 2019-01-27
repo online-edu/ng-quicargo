2a760965ed22624b8d1af57860fefc767a3c24dd
Hardik Pithva <hardikpthv@gmail.com>
2019-01-28 02:25:13 +0530

    fix: uglify issue

--- src/app/dashboard/dashboard.component.js
+++ src/app/dashboard/dashboard.component.js
@@ -7,28 +7,39 @@
 (function() {
   angular.module("quicargo").component("dashboard", {
     templateUrl: "partials/dashboard/dashboard.html",
-    controller: function DashboardController(dashboardService) {
-      var self = this;
+    controller: [
+      "dashboardService",
+      "$window",
+      function DashboardController(dashboardService, $window) {
+        var self = this;
+        $window.document.title = "Dashboard";
+        self.pallets = [
+          { id: 0, name: "Block pallet" },
+          { id: 1, name: "Stringer pallet" }
+        ];
+        self.countries = [];
+        self.dateOptions = {
+          formatYear: "yy",
+          maxDate: new Date(2020, 5, 22),
+          minDate: new Date(),
+          startingDay: 1
+        };
+        self.pickers = {
+          pickup: false,
+          delivery: false
+        };
+        self.loader = true;
+        self.slider = {
+          minValue: 0,
+          maxValue: 24,
+          options: {
+            floor: 0,
+            ceil: 24,
+            step: 0.5
+          }
+        };
 
-      self.user = "world";
-      self.pallets = [
-        { id: 0, name: "Block pallet" },
-        { id: 1, name: "Stringer pallet" }
-      ];
-      self.countries = [];
-      self.dateOptions = {
-        formatYear: "yy",
-        maxDate: new Date(2020, 5, 22),
-        minDate: new Date(),
-        startingDay: 1
-      };
-      self.pickers = {
-        pickup: false,
-        delivery: false
-      };
-      self.loader = true;
-
-      /* self.delivery = {
+        /* self.delivery = {
       route: {
         pickup: {
           city: "",
@@ -56,33 +67,33 @@
       }
     }; */
 
-      dashboardService.getCountries().then(function(data) {
-        self.countries = data;
-        self.loader = false;
-      });
+        dashboardService.getCountries().then(function(data) {
+          self.countries = data;
+          self.loader = false;
+        });
 
-      self.openPickupDate = function() {
-        self.pickers.pickup = true;
-        self.pickers.delivery = false;
-      };
+        self.openPickupDate = function() {
+          self.pickers.pickup = true;
+          self.pickers.delivery = false;
+        };
 
-      self.openDeliveryDate = function() {
-        self.pickers.delivery = true;
-        self.pickers.pickup = false;
-      };
+        self.openDeliveryDate = function() {
+          self.pickers.delivery = true;
+          self.pickers.pickup = false;
+        };
 
-      self.onSubmit = function(isValid) {
-        console.log(self.delivery, isValid);
-      };
+        self.onSubmit = function(isValid) {
+          console.log(self.delivery, isValid);
+        };
 
-      self.reset = function(form) {
-        if (form) {
-          form.$setPristine();
-          form.$setUntouched();
-        }
-      };
-
-      self.reset();
-    }
+        self.reset = function(form) {
+          if (form) {
+            form.$setPristine();
+            form.$setUntouched();
+          }
+        };
+        self.reset();
+      }
+    ]
   });
 })();
